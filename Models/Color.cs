e2d2aaf3b014f9f976607f5e4683acbd3f47e5b4
Carlos A Olivera <91080828+CarlosAOlivera@users.noreply.github.com>
2024-03-28 22:07:31 -0400

    Se añadieron Modelos Color, Talla, ProductoColor y ProductoTalla. Cambio StoreDB -> BoutiqueDB

---
+++ Models/Color.cs
@@ -0,0 +1,11 @@
+﻿namespace LionDev.Models
+{
+    public class Color
+    {
+        public Guid IdColor { get; set; }
+        public string Nombre { get; set; }
+
+        // Relación con ProductoColor
+        public ICollection<ProductoColor> ProductoColores { get; set; }
+    }
+}
--- obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a37ddecf2b792abd9a4128c30c0edfcd47bde9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe223c46c8b8764fb5412a45c5e62453e731e196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
