b7a9f2a8abfdcf96664093e55c17c37b9953cd19
Asariel <asarielalequin@icloud.com>
2024-05-26 07:02:08 -0400

    DEbug

--- obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f452962704eaf1d8d32bf47438c0120c5985a66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2280d2a0525577f4edb492f5655ba692f4166711")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
